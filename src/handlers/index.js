ffad4ab0c2ed70a976e6636b8387bc131cee8aaa
pyctem <pyctem7@gmail.com>
2023-01-06 22:09:22 +0500

    add post handler

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -34,6 +34,35 @@ export function getHandler (req, res) {
     return res.end(data);
 }
 
+export function postHandler (req, res) {
+    const { pathname } = url.parse(req.url);
+    const match = pathname.match(API_REGEX);
+
+    if (!match) {
+        return errorHandler(res, 404)
+    }
+
+    const [...other] = match[1]?.match(ID_REGEX) ?? [];
+
+    if (other.length) {
+        return errorHandler(res, 400);
+    }
+
+    const buffers = [];
+
+    req
+        .on('data', chunk => buffers.push(chunk))
+        .on('end', () => {
+            const buffer = Buffer.concat(buffers);
+            const uuid = crypto.randomUUID();
+
+            users.set(uuid, { ...JSON.parse(buffer.toString()), id: uuid });
+
+            res.setHeader('Content-Type', 'application/json;charset=utf-8');
+            res.end(`{"message": "User ${uuid} has added"}`);
+        })
+}
+
 function errorHandler (res, code) {
     res.statusCode = code;
     res.end(`<h1>${code}</h1><h2>${http.STATUS_CODES[code]}</h2>`);
