ebceca6a179bf1c72713d625ce388d6afbcf03a4
pyctem <pyctem7@gmail.com>
2023-01-06 22:03:05 +0500

    add get handler

---
+++ src/handlers/index.js
@@ -0,0 +1,35 @@
+import url from "node:url";
+import crypto from "node:crypto";
+import { Buffer } from "node:buffer";
+import { users } from "../data/index.js";
+import { API_REGEX, ID_REGEX } from "../constants.js";
+
+export function getHandler (req, res) {
+    const { pathname } = url.parse(req.url);
+    const match = pathname.match(API_REGEX);
+
+    if (!match) {
+        return errorHandler(res, 404);
+    }
+
+    const [id, ...other] = match[1]?.match(ID_REGEX) ?? [];
+
+    if (other.length) {
+        return errorHandler(res, 400);
+    }
+
+    if (id) {
+        const uuid = id.slice(1);
+        if (users.has(uuid)) {
+            res.setHeader('Content-Type', 'application/json;charset=utf-8');
+            return res.end(JSON.stringify(users.get(uuid)));
+        } else {
+            return errorHandler(res, 404);
+        }
+    }
+
+    const data = JSON.stringify(Array.from(users.values()));
+    
+    res.setHeader('Content-Type', 'application/json;charset=utf-8');
+    return res.end(data);
+}
