fc72cca6dcfd86e8f8ea65e35b9ade9ae00f05de
pyctem <pyctem7@gmail.com>
2023-01-08 19:41:24 +0500

    update constants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const API_REGEX = /\/api\/users(\/[\/\w\d-]*)*$/;
 export const ID_REGEX = /\/[\w\d-]+\/{0,1}?/g;
+export const UUID_REGEX = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/;
